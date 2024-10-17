fcc6da978d352a73cc90e96a8da5380e33c44bbb
Ian (TSG) <jhtsg1@gmail.com>
2024-10-17 19:36:24 -0400

    #5 Searches

--- Hue.Web/src/components/pages/artists/ArtistsPage.tsx
+++ Hue.Web/src/components/pages/artists/ArtistsPage.tsx
@@ -1,10 +1,10 @@
-import { Dialog, Fab, Tooltip } from "@mui/material"
+import { Button, Dialog, Fab, InputAdornment, TextField, Tooltip } from "@mui/material"
 import { getArtists } from "../../../api/Artist"
 import useApi from "../../hooks/useApi"
 import ApiAlert from "../../shared/ApiAlert"
 import LoadingBackdrop from "../../shared/LoadingBackdrop"
 import ArtistTile from "./subcomponents/ArtistTile"
-import { Add } from "@mui/icons-material"
+import { Add, Search } from "@mui/icons-material"
 import { useState } from "react"
 import ArtistPane from "./subcomponents/ArtistPane"
 import Artist from "../../../model/artist/Artist"
@@ -15,30 +15,61 @@ export default function ArtistsPage(props: {
     onSelect?: (val: Artist) => void
 }) {
 
+    const { onSelect } = props
+
     const [newOpen, setNewOpen] = useState(false)
+    const [search, setSearch] = useState('')
     const artistsApi = useApi(getArtists, true)
-    const { vertical } = useWindowDimensions();
+    const { vertical, maxComponentHeight } = useWindowDimensions();
     const { user } = useUser();
     const artist = user?.isArtist
 
     return <>
+
         <div style={{ display: "flex", alignItems: "end" }}>
-            <div style={{ fontSize: "1.7em", flex: "1" }}>{artist ? "Clients" : "Artist"}</div>
-        </div>
-        <hr />
-        <ApiAlert result={artistsApi.error} style={{ marginBottom: "20px" }} />
-        <div style={{ display: 'flex', flexWrap: 'wrap', width: '100%', marginTop: "20px", justifyContent: props.onSelect || vertical ? "center" : undefined }} >
-            {artistsApi.data?.map(a => <ArtistTile artist={a} onClick={props.onSelect ? () => {
-                props.onSelect?.(a)
-            } : undefined} />)}
+            {onSelect ? <div style={{ display: "flex", justifyContent: "space-between", width: "100%", alignItems: "center" }}>
+                <div style={{ flex: "1", marginRight: "40px" }}>
+                    <TextField variant="standard" placeholder={artist ? 'Search Clients' : 'Search Artists'} style={{ maxWidth: "400px" }} fullWidth value={search} onChange={(e) => setSearch(e.target.value)} slotProps={{
+                        input: {
+                            startAdornment: <InputAdornment position="start">
+                                <Search />
+                            </InputAdornment>
+                        }
+                    }} />
+                </div>
+                <div>
+                    <Button variant="contained" startIcon={vertical ? undefined : <Add />}>
+                        {vertical ? <Add /> : 'New Artist'}
+                    </Button>
+                </div>
+            </div> : <div style={{ fontSize: "1.7em", flex: "1" }}>{artist ? "Clients" : "Artist"}</div>}
         </div>
 
-        <Tooltip title={`Create a new ${artist ? 'client' : 'artist'}`}>
+        <hr />
+        <ApiAlert result={artistsApi.error} style={{ marginBottom: "20px" }} />
+        <div style={onSelect ? { overflowY: 'auto', height: maxComponentHeight - 200, } : undefined}>
+            <div style={{
+                display: 'flex', flexWrap: 'wrap',
+                width: '100%', marginTop: "20px",
+                justifyContent: props.onSelect || vertical ? "center" : undefined
+            }} >
+                {artistsApi.data?.filter((a) => search.trim().length === 0
+                    ? true
+                    : a.name.toLowerCase().includes(search.toLowerCase()) ||
+                    a.socialUrl.toLowerCase().includes(search.toLowerCase())
+                ).map(a => <ArtistTile artist={a} onClick={props.onSelect ? () => {
+                    props.onSelect?.(a)
+                } : undefined} />)}
+
+            </div>
+        </div>
+
+        {!onSelect && <Tooltip title={`Create a new ${artist ? 'client' : 'artist'}`}>
             <Fab color="primary" style={{ position: "fixed", bottom: "20px", right: "20px" }}
                 onClick={() => setNewOpen(true)} >
                 <Add />
             </Fab>
-        </Tooltip>
+        </Tooltip>}
 
         <LoadingBackdrop loading={artistsApi.loading} />
 
--- Hue.Web/src/components/pages/chars/CharsPage.tsx
+++ Hue.Web/src/components/pages/chars/CharsPage.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import useApi from "../../hooks/useApi";
 import { getCharacters } from "../../../api/Char";
-import { Dialog, Fab, Tooltip } from "@mui/material";
-import { Add } from "@mui/icons-material";
+import { Button, Dialog, Fab, InputAdornment, TextField, Tooltip } from "@mui/material";
+import { Add, Search } from "@mui/icons-material";
 import ApiAlert from "../../shared/ApiAlert";
 import LoadingBackdrop from "../../shared/LoadingBackdrop";
 import CharacterTile from "./subcomponents/CharacterTile";
@@ -14,37 +14,60 @@ export default function CharsPage(props: {
     onSelect?: (val: Character) => void
 }) {
 
+    const { onSelect } = props
+
     const [newOpen, setNewOpen] = useState(false)
-    const artistsApi = useApi(getCharacters, true)
-    const { vertical } = useWindowDimensions();
+    const [search, setSearch] = useState('')
+    const charactersApi = useApi(getCharacters, true)
+    const { vertical, maxComponentHeight } = useWindowDimensions();
 
     return <>
         <div style={{ display: "flex", alignItems: "end" }}>
-            <div style={{ fontSize: "1.7em", flex: "1" }}>Characters</div>
+            {onSelect ? <div style={{ display: "flex", justifyContent: "space-between", width: "100%", alignItems: "center" }}>
+                <div style={{ flex: "1", marginRight: "40px" }}>
+                    <TextField variant="standard" placeholder="Search Characters" style={{ maxWidth: "400px" }} fullWidth value={search} onChange={(e) => setSearch(e.target.value)} slotProps={{
+                        input: {
+                            startAdornment: <InputAdornment position="start">
+                                <Search />
+                            </InputAdornment>
+                        }
+                    }} />
+                </div>
+                <div>
+                    <Button variant="contained" startIcon={vertical ? undefined : <Add />}>
+                        {vertical ? <Add /> : 'New Character'}
+                    </Button>
+                </div>
+            </div> : <div style={{ fontSize: "1.7em", flex: "1" }}>Characters</div>}
         </div>
         <hr />
-        <ApiAlert result={artistsApi.error} style={{ marginBottom: "20px" }} />
-        <div style={{ display: 'flex', flexWrap: 'wrap', width: '100%', marginTop: "20px", justifyContent: props.onSelect || vertical ? "center" : undefined }} >
-            {artistsApi.data?.map(a => <CharacterTile character={a} onClick={props.onSelect ? () => {
-                props.onSelect?.(a)
-            } : undefined} />)}
+        <ApiAlert result={charactersApi.error} style={{ marginBottom: "20px" }} />
+        <div style={onSelect ? { overflowY: 'auto', height: maxComponentHeight - 200, } : {}}>
+            <div style={{ display: 'flex', flexWrap: 'wrap', width: '100%', marginTop: "20px", justifyContent: props.onSelect || vertical ? "center" : undefined }} >
+                {charactersApi.data?.filter((a) => search.trim().length === 0
+                    ? true
+                    : a.name.toLowerCase().includes(search.toLowerCase())
+                ).map(a => <CharacterTile character={a} onClick={props.onSelect ? () => {
+                    props.onSelect?.(a)
+                } : undefined} />)}
+            </div>
         </div>
 
-        <Tooltip title="Create a new Character">
+        {!onSelect && <Tooltip title="Create a new Character">
             <Fab color="primary" style={{ position: "fixed", bottom: "20px", right: "20px" }}
                 onClick={() => setNewOpen(true)} >
                 <Add />
             </Fab>
-        </Tooltip>
+        </Tooltip>}
 
 
-        <LoadingBackdrop loading={artistsApi.loading} />
+        <LoadingBackdrop loading={charactersApi.loading} />
 
         <Dialog open={newOpen} onClose={() => setNewOpen(false)} maxWidth="md" fullWidth>
             <div style={{ padding: 20 }}>
                 <CharacterPane create open={newOpen} setOpen={setNewOpen} onOk={() => {
                     setNewOpen(false);
-                    artistsApi.fetch();
+                    charactersApi.fetch();
                 }} />
 
             </div>
