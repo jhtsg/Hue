05fa5efd69fdd339faa46cc5f8f136389a40b639
jhtsg <jhtsg1@gmail.com>
2024-10-18 17:17:23 -0400

    PWA and meta updates

--- Hue.Web/vite.config.ts
+++ Hue.Web/vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    includeAssets: ['favicon.ico', 'icon180.png'],
+    manifest: {
+      name: 'Hue',
+      short_name: 'Hue',
+      description: "Hue is your new home for commission management, planning, and tracking. See where your commissions stand in one easy board. Track the artists you've worked with, their works, and their prices. See your characters and how much you've commissioned for them. All in one place!",
+      theme_color: '#5BA56A',
+      icons: [
+        {
+          src: 'icon192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'icon512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })],
 })
