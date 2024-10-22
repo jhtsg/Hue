ac40acd9702952320e275071318c971a9c97fc19
jhtsg <jhtsg1@gmail.com>
2024-10-21 20:56:24 -0400

    Change max age to be tiny

--- Hue.API/Controllers/CommController.cs
+++ Hue.API/Controllers/CommController.cs
@@ -138,7 +138,7 @@ namespace Hue.API.Controllers
             if (file== null || file.Data==null || file.Mime==null) return NotFound();
                         
             Response.Headers.Append("Content-Disposition", "inline; filename=" + new string(file.FullFilename.Where(c => c < 128).ToArray()));
-            Response.Headers.CacheControl = "public, max-age=31536000";
+            Response.Headers.CacheControl = "public, max-age=10";
             Response.Headers.Vary = "Cookie";
             Response.Headers.ETag = file.Hash;
 
