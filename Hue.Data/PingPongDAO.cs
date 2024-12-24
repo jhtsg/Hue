51df58be3a39951273622b53b90e79108873de3d
Ian (TSG) <jhtsg1@gmail.com>
2024-12-23 22:49:40 -0400

    #23 Backend work

---
+++ Hue.Data/PingPongDAO.cs
@@ -0,0 +1,35 @@
+﻿using Hue.Data.Utils;
+
+namespace Hue.Data {
+    public class PingPongDao(string connectionString) {
+        readonly AdoTemplate adoTemplate = new(connectionString);
+
+        public class DbPingPong {
+            public DateTime PingTime { get; set; }
+            public DateTime? PongTime { get; set; }
+            public bool Up => PongTime!=null;
+        }
+
+        #region READ
+
+        public async Task<DbPingPong> PingPong() {
+
+            DateTime pingTime = DateTime.UtcNow;
+            var sql = "select CURRENT_TIMESTAMP";
+            DateTime? pongTime;
+
+            try {
+                pongTime = await adoTemplate.QuerySingle(sql, (cmd) => { }, (reader) => reader.GetDateTime(0));
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+                pongTime = null;
+            }
+
+            return new DbPingPong { PingTime = pingTime, PongTime = pongTime };
+            
+        }
+
+        #endregion
+
+    }
+}
